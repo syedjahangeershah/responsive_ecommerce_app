4151ec8f13b03f38fa625b08ee409f86911fbbd3
Syed Jahangeer Shah <syedjahangeershah148@gmail.com>
2023-03-18 14:49:06 +0500

    provider implementation started

---
+++ lib/src/features/products/data/fake_products_repository.dart
@@ -0,0 +1,33 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FakeProductsRepository {
+
+  final List<Product> _products = kTestProducts;
+
+  List<Product> getProductsList() {
+    return _products;
+  }
+
+  Product? getProduct(String id) {
+    return _products.firstWhere((product) => product.id == id);
+  }
+
+  Future<List<Product>> fetchProductsList() {
+    return Future.value(_products);
+  }
+
+  Stream<List<Product>> watchProductsList() {
+    return Stream.value(_products);
+  }
+
+  Stream<Product?> watchProduct(String id) {
+    return watchProductsList()
+        .map((products) => products.firstWhere((product) => product.id == id));
+  }
+}
+
+final productRepositoryProvider = Provider<FakeProductsRepository>((ref) {
+  return FakeProductsRepository();
+});
